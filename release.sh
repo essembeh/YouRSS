64756d5272eb3b88a907cee30ad3a88a7e13ad2a
Sébastien MB <seb@essembeh.org>
2024-01-12 14:46:37 +0100

    Update github CI

---
+++ release.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+[ "$1" = "patch" ] || [ "$1" = "minor" ] || [ "$1" = "major" ]
+
+# create new version with poetry
+VERSION=$(poetry version "$1" -s)
+
+# update charts
+yq e '.version = strenv(VERSION)'    -i charts/yourss/Chart.yaml
+yq e '.appVersion = strenv(VERSION)' -i charts/yourss/Chart.yaml
+
+# add updated files to staging
+git add pyproject.toml charts/yourss/Chart.yaml
+git --no-pager diff --staged 
+
+echo ""
+echo ""
+echo ""
+
+# ask confirmation before commit
+read -p "💡 Commit changes and create tag? [y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git commit --message "🔖 New release: $VERSION"
+    git tag "$VERSION"
+
+    # ask confirmation before push to origin
+    read -p "💡 Push to origin? [y/n] " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        git push 
+        git push --tags
+    fi
+else
+    # revert changes
+    echo "Reverting changes ..."
+    git reset HEAD pyproject.toml charts/yourss/Chart.yaml
+    git checkout pyproject.toml charts/yourss/Chart.yaml
+fi
